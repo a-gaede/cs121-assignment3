c770694f684d6fc55618c59199b9b8cd255aa253
eangeric <130268209+eangeric@users.noreply.github.com>
2024-03-01 18:21:51 -0800

    Add docID mapping

--- search.py
+++ search.py
@@ -3,5 +3,7 @@ from retriever import Retriever
 if __name__ == "__main__":
     # Put path of index file
     invertedIndexFile = 'reports\invertedIndex.json'
-    retriever = Retriever(invertedIndexFile)
+    # Put path of docID mapping file
+    docMapping = 'reports\docMapping.json'
+    retriever = Retriever(invertedIndexFile, docMapping)
     retriever.retrieve()
