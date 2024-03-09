753a4dd2623f3bd4d77fc702e2257f58dbc38b6f
eangeric <130268209+eangeric@users.noreply.github.com>
2024-03-09 06:47:52 -0800

    Update retriever.py

--- retriever.py
+++ retriever.py
@@ -1,68 +1,103 @@
 import json
+import math
+from nltk.stem import PorterStemmer
 from functools import reduce
+from collections import defaultdict
 
 
 class Retriever:
 
-    # pass in inverted-index for comparison
-    def __init__(self, index, docMap):
-        with open(index, 'r') as indexFile:
-            self.invertedIndex = json.load(indexFile)
-        with open(docMap, 'r') as docMapFile:
-            self.docMapping = json.load(docMapFile)
+    # pass in inverted-index directory and doc map for comparison
+    def __init__(self, path, totalNum, docMapping):
+        self.stemmer = PorterStemmer()
+        self.invertedIndexPath = path
+        with open(totalNum, "r") as total:
+            self.total = int(total.read())
+        with open(docMapping, "r") as docMapFile:
+            self.docMap = json.load(docMapFile)
 
     # individual query token acquisition
     def getQueryTokens(self):
         query = input("Enter Query: ")
-        queryTokens = self.tokenize(query)
-        return queryTokens
+        return [self.stemmer.stem(word) for word in query.split()]
 
-    # tokenize method from indexConstruction
-    def tokenize(self, text):
-        tokens = []
-        token = ""
-        for char in text.lower():
-            if char.isalpha():
-                token += char
+    def computeMergedPostings(self, tokens):
+        mergedPostings = {}
+        for token in tokens:
+            with open(f"{self.invertedIndexPath}/Index-{token[0]}.json") as II:
+                data = json.load(II)
+                if token in data:
+                    mergedPostings[token] = data[token]
+
+        return dict(sorted(mergedPostings.items(), key=lambda item: len(item[1])))
+
+    # Return dict of the lowest term in format
+    # docID : postings
+    def computeLowestPostings(self, lowest):
+        lowestPostings = {}
+        for post in lowest:
+            docID = post[0]
+            if docID in lowestPostings:
+                lowestPostings[docID].append(post)
             else:
-                if token:
-                    tokens.append(token)
-                    token = ""
-        if token:
-            tokens.append(token)
-        return tokens
+                lowestPostings[docID] = [post]
+
+        return lowestPostings
+
+    # Return dict of AND terms in format
+    # doc ID : postings
+    def computeAND(self, merged):
+        if merged:
+            # Term with lowest document frequency
+            lowest = min(merged, key=lambda term: len(merged[term]))
+            # Get the docIDs from lowest
+            docIDs = {post[0] for post in merged[lowest]}
+            # Store AND posts
+            ANDPostings = self.computeLowestPostings(merged[lowest])
+
+            for token in list(merged.keys())[1:]:
+                for post in merged[token]:
+                    if post[0] in docIDs:
+                        ANDPostings[post[0]].append(post)
+
+            return ANDPostings
+        return {}
+
+    def computeTFIDF(self, post, df):
+        return (post[1] + math.log10(post[2])) * math.log10(self.total / df)
+
+    # Scores are computed as follows
+    # (FIELD WEIGHT + log(tf)) * log(N / df)
+    def computeScores(self, postings):
+        scores = defaultdict(int)
+
+        for docID in postings:
+            df = len(postings[docID])
+            scores[docID] = sum(
+                [self.computeTFIDF(post, df) for post in postings[docID]]
+            )
+
+        uniqueScores = {v: k for k, v in scores.items()}
+
+        return uniqueScores
 
     # loop repeatedly taking queries until 'quit'
     def retrieve(self):
         queryTag = True
         while queryTag:
-            query = self.getQueryTokens()
-            if query[0] == 'quit':
+            tokens = self.getQueryTokens()
+            if tokens[0] == "quit":
                 break
-            print(query)
-            results = self.findTokenCounts(query)
-            if len(results) == 0:
+            merged = self.computeMergedPostings(tokens)
+            ANDPostings = self.computeAND(merged)
+            scores = self.computeScores(ANDPostings)
+            if len(scores) == 0:
                 print("No results")
                 continue
-            print('Results:')
-            for docID in results:
-                print(f'\t{self.docMapping[str(docID)]}')
-
-    # Does AND operation
-    def findTokenCounts(self, queryTokens):
-        found = []
-
-        if len(queryTokens) == 1:
-            if queryTokens[0] in self.invertedIndex:
-                found = self.invertedIndex[queryTokens[0]][0]
-        else:
-            for query in queryTokens:
-                if query in self.invertedIndex:
-                    found.append(self.invertedIndex[query][0])
-            if len(found) != 0:
-                found = list(reduce(set.intersection, map(set, found)))
-
-        if len(found) >= 5:
-            return found[:5]
-        else:
-            return found
+            print("Results:")
+            print(f"#\tScore\tURL")
+            sortedScores = dict(
+                sorted(scores.items(), key=lambda item: item[0], reverse=True)
+            )
+            for i, score in enumerate(sortedScores):
+                print(f"{i+1}\t{round(score)}\t{self.docMap[str(sortedScores[score])]}")
