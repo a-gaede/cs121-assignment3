65c13d10437c24c849265d5d83b4058339050516
eangeric <130268209+eangeric@users.noreply.github.com>
2024-03-09 06:47:34 -0800

    Update indexConstruction.py

--- indexConstruction.py
+++ indexConstruction.py
@@ -1,13 +1,31 @@
 import json
 import os
+import re
 from bs4 import BeautifulSoup
 from nltk.stem import PorterStemmer
+from collections import defaultdict
+from urllib.parse import urlparse
+
+"""
+FIELD KEY
+
+1: p
+2: b and strong
+3: h6
+4: h5
+5: h4
+6: h3
+7: h2
+8: h1
+9: title
+
+"""
 
 
 class InvertedIndex:
     def __init__(self):
         # Dictionary to hold postings
-        self.invertedIndex = {}
+        self.invertedIndex = defaultdict(list)
         # Holds number of indexed documents
         self.indexedDocuments = 0
         # Holds mapping for docIDs
@@ -15,37 +33,75 @@ class InvertedIndex:
         # Batch size
         self.batchSize = 2
         # File counter
-        self.fileCounter = 0
+        self.fileCounter = 1
         # Batch counter
         self.batchCounter = 1
+        # Stemmer
+        self.stemmer = PorterStemmer()
+        # Count for IDs
+        self.count = 1
 
     """OPEN FILE AND GET TEXT"""
 
     # Open HTML json file
     def openHTML(self, path):
-        with open(path, 'r') as HTMLjson:
+        with open(path, "r") as HTMLjson:
             data = json.load(HTMLjson)
 
         return data
 
     # Get HTML text
     def getText(self, dataContent):
-        soup = BeautifulSoup(dataContent, 'html.parser')
-        text = {
-            'p': ' '.join([p.get_text() for p in soup.find_all('p')]),
-            'bold': ' '.join([bold.get_text() for bold in soup.find_all(['b', 'strong'])]),
-            'h1': ' '.join([h1.get_text() for h1 in soup.find_all('h1')]),
-            'h2': ' '.join([h2.get_text() for h2 in soup.find_all('h2')]),
-            'h3': ' '.join([h3.get_text() for h3 in soup.find_all('h3')]),
-            'h4': ' '.join([h4.get_text() for h4 in soup.find_all('h4')]),
-            'h5': ' '.join([h5.get_text() for h5 in soup.find_all('h5')]),
-            'h6': ' '.join([h6.get_text() for h6 in soup.find_all('h6')]),
-            'title': ' '.join([title.get_text() for title in soup.find_all('title')])
-        }
+        if dataContent.strip():
+            soup = BeautifulSoup(dataContent, "html.parser")
+            text = {
+                1: [
+                    self.stemTokens(self.tokenize(p.get_text()))
+                    for p in soup.find_all("p")
+                ],
+                2: [
+                    self.stemTokens(self.tokenize(bold.get_text()))
+                    for bold in soup.find_all(["b", "strong"])
+                ],
+                3: [
+                    self.stemTokens(self.tokenize(h6.get_text()))
+                    for h6 in soup.find_all("h6")
+                ],
+                4: [
+                    self.stemTokens(self.tokenize(h5.get_text()))
+                    for h5 in soup.find_all("h5")
+                ],
+                5: [
+                    self.stemTokens(self.tokenize(h4.get_text()))
+                    for h4 in soup.find_all("h4")
+                ],
+                6: [
+                    self.stemTokens(self.tokenize(h3.get_text()))
+                    for h3 in soup.find_all("h3")
+                ],
+                7: [
+                    self.stemTokens(self.tokenize(h2.get_text()))
+                    for h2 in soup.find_all("h2")
+                ],
+                8: [
+                    self.stemTokens(self.tokenize(h1.get_text()))
+                    for h1 in soup.find_all("h1")
+                ],
+                9: [
+                    self.stemTokens(self.tokenize(title.get_text()))
+                    for title in soup.find_all("title")
+                ],
+            }
 
-        self.indexedDocuments += 1
+            # Flattening the nested lists
+            for key in text:
+                text[key] = [item for sublist in text[key] for item in sublist]
 
-        return text
+            self.indexedDocuments += 1
+
+            return text
+        else:
+            return None
 
     """TOKENIZE AND STEM"""
 
@@ -54,36 +110,19 @@ class InvertedIndex:
         tokens = []
         token = ""
         for char in text.lower():
-            if char.isalpha():
+            # Check if an english alphabet character or number
+            if 97 <= ord(char) <= 122 or char.isnumeric():
                 token += char
             else:
                 if token:
                     tokens.append(token)
                     token = ""
-        if token:
-            tokens.append(token)
+
         return tokens
 
     # Stem tokens
-    def stem(self, tokens):
-        stemmer = PorterStemmer()
-        tokens = [stemmer.stem(token) for token in tokens]
-
-        return tokens
-
-    """PERFORM TEXT PROCESSING TASKS ON HTML CONTENT"""
-
-    # Process
-    def textProcessing(self, dataContent):
-        text = self.getText(dataContent)
-        tokens = {}
-
-        for field in text:
-            fieldTokens = self.tokenize(text[field])
-            stemmedFieldTokens = self.stem(fieldTokens)
-            tokens[field] = stemmedFieldTokens
-
-        return tokens
+    def stemTokens(self, tokens):
+        return [self.stemmer.stem(token) for token in tokens]
 
     """CREATE POSTINGS AND CREATE INDEX"""
 
@@ -98,46 +137,66 @@ class InvertedIndex:
 
         return documentTokenFrequency
 
+    # Calculate term frequency scores within document
+    def computeTermFrequencyScores(self, tokens):
+        frequencies = self.computeFrequencies(tokens)
+        totalTokens = sum(frequencies.values())
+        return {token: freq / totalTokens for token, freq in frequencies.items()}
+
     # Add to dictionary in format
     # token: [[posting1], [posting2], [posting3], etc...]
     # where posting is in format
     # [docID, field, frequency]
-    def computePostings(self, documentID, field, frequencies):
-        for token in frequencies:
-            if token in self.invertedIndex:
-                self.invertedIndex[token].append(
-                    [documentID, field, frequencies[token]])
-            else:
-                self.invertedIndex[token] = [
-                    [documentID, field, frequencies[token]]]
+    def computePostings(self, documentID, field, termFrequencies):
+        for token, freq in termFrequencies.items():
+            self.invertedIndex[token].append([documentID, field, freq])
+
+    # CHECK URL VALID
+    def isValid(self, URL):
+        parsed = urlparse(URL)
+        return not re.match(
+            r".*\.(css|js|bmp|gif|jpe?g|ico"
+            + r"|png|tiff?|mid|mp2|mp3|mp4"
+            + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf|txt"
+            + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names|ppsx"
+            + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
+            + r"|epub|dll|cnf|tgz|sha1"
+            + r"|thmx|mso|arff|rtf|jar|csv"
+            + r"|rm|smil|wmv|swf|wma|zip|rar|gz|odc)$",
+            parsed.path.lower(),
+        )
 
     # Add the postings to the index given file
-    def index(self, dirPath, fileName, count):
+    def index(self, dirPath, fileName, dataID):
         data = self.openHTML(dirPath + "/" + fileName)
-        dataContent = data['content']
-        URL = data['url']
-        dataID = count
+        dataContent = data["content"]
+        URL = data["url"]
 
-        self.docMapping[dataID] = URL
+        if self.isValid(URL):
+            print(f"Processing doc {self.count}")
+            self.docMapping[dataID] = URL
 
-        tokens = self.textProcessing(dataContent)
-        frequencies = {}
+            tokens = self.getText(dataContent)
+            if tokens != None:
+                termFrequencies = {}
 
-        for field in tokens:
-            frequencies[field] = self.computeFrequencies(tokens[field])
+                for field in tokens:
+                    termFrequencies[field] = self.computeTermFrequencyScores(
+                        tokens[field]
+                    )
 
-        for field in frequencies:
-            self.computePostings(
-                dataID, field, frequencies[field])
+                for field in termFrequencies:
+                    self.computePostings(dataID, field, termFrequencies[field])
+
+            self.count += 1
+            self.fileCounter += 1
 
     def createBatchIndexes(self, dirPath, fileNames, count):
-        self.fileCounter += 1
-
         if self.fileCounter >= (self.batchSize):
             self.index(dirPath, fileNames, count)
             self.writeIIBatchesToJson(self.batchCounter)
-            self.invertedIndex = {}
-            self.fileCounter = 0
+            self.invertedIndex = defaultdict(list)
+            self.fileCounter = 1
             self.batchCounter += 1
         else:
             self.index(dirPath, fileNames, count)
@@ -146,28 +205,35 @@ class InvertedIndex:
 
     # Create json of inverted index
     def writeIIBatchesToJson(self, batchNumber):
-        jsonStructure = {token: [(docID, field, freq) for docID, field, freq in postings]
-                         for token, postings in self.invertedIndex.items()}
+        jsonStructure = {
+            token: [(docID, field, freq) for docID, field, freq in postings]
+            for token, postings in self.invertedIndex.items()
+        }
 
-        with open(f'reports/InvertedIndexReports/IIBatch{batchNumber}.json', 'w') as invertedIndexJson:
+        with open(
+            f"reports/InvertedIndexReports/IIBatch{batchNumber}.json", "w"
+        ) as invertedIndexJson:
             json.dump(jsonStructure, invertedIndexJson)
 
     def writeDocMapping(self):
-        with open('reports/docMapping.json', 'w') as docMapping:
+        with open("reports/docMapping.json", "w") as docMapping:
             json.dump(self.docMapping, docMapping)
+
+    def writeTotalDocsIndexed(self):
+        with open("reports/docsIndexed.txt", "w") as docsIndexed:
+            json.dump(self.indexedDocuments, docsIndexed)
+
+    def start(self, ROOT):
+        for dirPath, _, fileNames in os.walk(ROOT):
+            for fileName in fileNames:
+                self.createBatchIndexes(dirPath, fileName, self.count)
+
+        if self.invertedIndex:
+            self.writeIIBatchesToJson(self.batchCounter)
+        self.writeDocMapping()
 
 
 if __name__ == "__main__":
-    # ADD YOUR DIRECTORY ROOT HERE FOR YOUR WEBPAGES
     ROOT = r"test"
-    count = 0
     InvertedIndex = InvertedIndex()
-    for dirPath, _, fileNames in os.walk(ROOT):
-        for fileName in fileNames:
-            count += 1
-            print(f'Processing Doc {count}')
-            InvertedIndex.createBatchIndexes(dirPath, fileName, count)
-    if InvertedIndex.invertedIndex:
-        InvertedIndex.writeIIBatchesToJson(
-            InvertedIndex.batchCounter)
-    InvertedIndex.writeDocMapping()
+    InvertedIndex.start(ROOT)
