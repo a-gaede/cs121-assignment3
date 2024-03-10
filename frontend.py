71eb9d6b53c1b1e43cffaddae682660f65af5719
Adam Gaede <agaede@uci.edu>
2024-03-09 22:38:52 -0800

    Updated Frontend.py

--- frontend.py
+++ frontend.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 from search import Search
 
 # Default name for flask
@@ -9,11 +9,13 @@ app = Flask(__name__)
 @app.route('/',methods=['POST', 'GET'])
 def run_search():
     # Fill html for landing page
-    src = '<html><body><style>form{background-color: lightblue;padding:5px;}</style><form action = "http://localhost:5000/" method = "post"><p>Search The ICS Webserver:</p><p><input type="text" name = "query" placeholder="Search..." /></p><p><input type = "submit" value = "Submit" /></p></form></body></html>'
-    query = request.form['query'] # Get query from form
-    raw_results = Search(query) # Search for results
-    results = src +"<pre>" + raw_results + "</pre>"
-    return results
+    if request.method == 'POST':
+        query = request.form['query'] # Get query from form
+        raw_results = Search(query) # Search for results
+        results = render_template("search.html") +"<pre>" + raw_results + "</pre>"
+        return results
+    elif request.method == 'GET':
+        return render_template("search.html")
 
  
 # main driver function
